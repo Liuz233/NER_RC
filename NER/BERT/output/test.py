e6894807c99d1f04ef676ce177905cb8797bc400
Liuz233 <92071158+Liuz233@users.noreply.github.com>
2024-06-30 23:04:50 +0800

    Delete NER directory

--- NER/BERT/output/test.py
+++
@@ -1,8 +0,0 @@
-with open("lstm.txt","r") as f:
-    with open("lstm_0.txt","w") as f_out:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            line = line.replace(",","/")
-            line+="/\n"
-            f_out.write(line)
